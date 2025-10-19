1ed791f8320517e2a87459be2023486ffe558326
Berend Klein Haneveld <berend.kleinhaneveld@zimmerbiomet.com>
2025-10-19 23:13:02 +0200

    Fix for parsing cgx files without template nodes

--- ruff_cgx/formatter.py
+++ ruff_cgx/formatter.py
@@ -12,6 +12,9 @@ logger = logging.getLogger(__name__)
 
 
 def format_script(path, script_node, source_lines, check):
+    """
+    Returns formatted source and the original location of the node
+    """
     start, end = script_node.location[0], script_node.end[0] - 1
 
     source = "".join(source_lines[start:end])
@@ -66,29 +69,48 @@ def format_file(path, check=False, write=True):
         lines = fh.readlines()
 
     script_node = parser.root.child_with_tag("script")
-    template_node = parser.root.child_with_tag("template")
+    template_nodes = [
+        node
+        for node in parser.root.children
+        if not hasattr(node, "tag") or node.tag != "script"
+    ]
 
     script_content, script_location = format_script(path, script_node, lines, check)
-    template_content, template_location = format_template(
-        template_node, lines, parser=parser
-    )
+    formatted_template_nodes = [
+        format_template(node, lines, parser=parser) for node in template_nodes
+    ]
 
-    changed = (
-        lines[script_location[0] : script_location[1]] != script_content
-        or lines[template_location[0] : template_location[1]] != template_content
+    changed_script = lines[script_location[0] : script_location[1]] != script_content
+    changed_template = any(
+        [
+            lines[template_location[0] : template_location[1]] != template_content
+            for template_content, template_location in formatted_template_nodes
+        ]
     )
+    needs_newline_at_end_of_file = not lines[-1].endswith("\n")
+    changed = changed_script or changed_template or needs_newline_at_end_of_file
     if check:
         if changed:
             logger.warning(f"Would change: {path}")
             return 1
         return 0
 
-    if script_location[0] > template_location[0]:
-        lines[script_location[0] : script_location[1]] = script_content
-        lines[template_location[0] : template_location[1]] = template_content
-    else:
-        lines[script_location[0] : script_location[1]] = script_content
-        lines[template_location[0] : template_location[1]] = template_content
+    formatted_parts = reversed(
+        sorted(
+            [
+                (script_content, script_location),
+                *formatted_template_nodes,
+            ],
+            key=lambda x: x[1][0],
+        )
+    )
+
+    for formatted_content, (start, end) in formatted_parts:
+        lines[start:end] = formatted_content
+
+    if needs_newline_at_end_of_file:
+        lines.append("\n")
+
     if not write:
         return lines
 
