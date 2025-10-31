e07ac2b56bf0dc2c157c3dbf9ccec3ca0bfb9efc
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2025-10-31 10:28:40 +0100

    Make it possible to configure ruff command

--- ruff_cgx/__init__.py
+++ ruff_cgx/__init__.py
@@ -6,10 +6,18 @@ from .linter import (
     lint_cgx_content,
     lint_file,
 )
+from .utils import (
+    get_ruff_command,
+    reset_ruff_command,
+    set_ruff_command,
+)
 
 __all__ = (
     "format_cgx_content",
     "format_file",
+    "get_ruff_command",
     "lint_cgx_content",
     "lint_file",
+    "reset_ruff_command",
+    "set_ruff_command",
 )
